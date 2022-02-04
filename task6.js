beb6f5b7571bf66ba1294b254aecd0363e39be3e
gmatik <merhabaxd1453@gmail.com>
2022-02-04 21:15:27 +0300

    fonksiyon

--- task6.js
+++ task6.js
@@ -1,3 +1,21 @@
 console.log('1937937')
 console.log(Math.round(Math.random() * 100))
+function kisi(İsim,Yaş,Sayı) {
+   const adam = {
+    ad:"Ziyalo Tehlike",
+    yas:34,
+    No: 20731,
+  };
+return adam;
+ }
+ let adam = (kisi())
+ console.log(adam);
+
+
+
+
+
+
+
+
 
